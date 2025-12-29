7e46dd58abd2e810767bfeaeb68fab9e68d0410d
Guigdsdev <guigdsdev@gmail.com>
2025-12-28 23:23:30 -0300

    feat: Cria ClientDTO para controle de estrutura e exposição de dados

--- src/main/java/dev/gui/supermarket/DTOs/ClientDTO.java
+++ src/main/java/dev/gui/supermarket/DTOs/ClientDTO.java
@@ -1,4 +1,24 @@
 package dev.gui.supermarket.DTOs;
 
+import dev.gui.supermarket.model.ProductModel;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String image;
+    private String address;
+    private List<ProductModel> products;
 }
