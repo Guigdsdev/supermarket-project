93f04f3067e0683937ca666a92fccec443fa974f
Guigdsdev <guigdsdev@gmail.com>
2025-12-28 23:06:50 -0300

    feat: Cria modelagem das entidades Client e Product

--- src/main/java/dev/gui/supermarket/model/ClientModel.java
+++ src/main/java/dev/gui/supermarket/model/ClientModel.java
@@ -2,9 +2,11 @@ package dev.gui.supermarket.model;
 
 import jakarta.persistence.*;
 
-@Table(name = "TB_CLIENTE")
+import java.util.List;
+
+@Table(name = "TB_CLIENT")
 @Entity
-public class ClienteModel {
+public class ClientModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,13 +15,18 @@ public class ClienteModel {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "age")
     private Integer age;
 
+    @Column(unique = true, name = "email")
     private String email;
 
+    @Column(name = "image")
     private String image;
 
+    @Column(name = "address")
     private String address;
 
-    
+    @OneToMany(mappedBy = "client")
+    private List<ProductModel> products;
 }
--- src/main/java/dev/gui/supermarket/model/ProductModel.java
+++ src/main/java/dev/gui/supermarket/model/ProductModel.java
@@ -1,4 +1,35 @@
 package dev.gui.supermarket.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_PRODUCT")
 public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "mark")
+    private String mark;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(unique = true, name = "uniqueCode")
+    private Integer uniqueCode;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private ClientModel client;
 }
