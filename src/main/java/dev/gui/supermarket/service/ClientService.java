47d072fbd319927af55575b237a0643074f83053
Guigdsdev <guigdsdev@gmail.com>
2026-01-09 01:20:56 -0300

    feat: Cria ClientService com as principais funcionalidades

--- src/main/java/dev/gui/supermarket/service/ClientService.java
+++ src/main/java/dev/gui/supermarket/service/ClientService.java
@@ -1,8 +1,52 @@
 package dev.gui.supermarket.service;
 
+import ch.qos.logback.core.net.server.Client;
+import dev.gui.supermarket.DTOs.ClientDTO;
+import dev.gui.supermarket.mapper.ClientMapper;
+import dev.gui.supermarket.model.ClientModel;
+import dev.gui.supermarket.repository.ClientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
-public class ClienteService {
+public class ClientService {
+
+    private final ClientRepository repository;
+
+    private final ClientMapper mapper;
+
+    public ClientService(ClientRepository repository, ClientMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+
+
+    public List<ClientDTO> listAllClients(){
+        List<ClientModel> client = repository.findAll();
+        return client.stream().map(mapper::map).collect(Collectors.toList());
+    }
+
+    public ClientDTO createClient(ClientDTO clientDTO){
+       ClientModel client = mapper.map(clientDTO);
+       repository.save(client);
+       return mapper.map(client);
+    }
+
+    public ClientDTO editClient(Long id, ClientDTO clientDTO){
+        Optional<ClientModel> client = repository.findById(id);
+        return client.map(mapper::map).orElse(null);
+    }
+
+    public ClientDTO removeClient(Long id){
+        if(repository.findById(id) != null){
+            repository.deleteById(id);
+        }
+        return null;
+    }
+
 
 }
